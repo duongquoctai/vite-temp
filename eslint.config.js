183076190faa2dc403edebeddcf9ab8e33eadbf3
Trevor <trevor@192.168.1.2.non-exists.ptr.local>
2025-04-08 20:14:04 +0700

    update

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      "no-console": "warn",
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
